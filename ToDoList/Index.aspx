77c1df206f549ed652e99fed53d820e9c5cf847d
Naylin88 <naylinoo2009@gmail.com>
2020-06-09 19:00:52 +0630

    Add project files.

---
+++ ToDoList/Index.aspx
@@ -0,0 +1,46 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="ToDoList.Index" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <style type="text/css">
+        #TextArea1 {
+            height: 300px;
+            width: 400px;
+        }
+    </style>
+</asp:Content>
+    
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <asp:Panel ID="Panel1" runat="server" style="float: left; width: 50%; display: inline;">
+    <br />
+    <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+    <asp:ImageButton ID="ImageButton1" runat="server" OnClick="ImageButton1_Click" ImageUrl="~/Images/Calender.jpg" />
+    <br />
+    <asp:Calendar ID="Calendar1" runat="server" BackColor="White" BorderColor="#999999" CellPadding="4" DayNameFormat="Shortest" Font-Names="Verdana" Font-Size="8pt" ForeColor="Black" Height="180px" Width="200px" OnSelectionChanged="SelectedEvent">
+        <DayHeaderStyle BackColor="#CCCCCC" Font-Bold="True" Font-Size="7pt" />
+        <NextPrevStyle VerticalAlign="Bottom" />
+        <OtherMonthDayStyle ForeColor="#808080" />
+        <SelectedDayStyle BackColor="#666666" Font-Bold="True" ForeColor="White" />
+        <SelectorStyle BackColor="#CCCCCC" />
+        <TitleStyle BackColor="#999999" BorderColor="Black" Font-Bold="True" />
+        <TodayDayStyle BackColor="#CCCCCC" ForeColor="Black" />
+        <WeekendDayStyle BackColor="#FFFFCC" />
+    </asp:Calendar>
+    <br />
+        <asp:TextBox ID="TextBox2" runat="server" BorderColor="#FF3300" BorderStyle="Groove" Height="300px" TextMode="MultiLine" Width="400px"></asp:TextBox>
+    <br />
+    <br />
+    <asp:Button runat="server" Text="SAVE" OnClick="Unnamed2_Click" BorderStyle="Groove" BorderColor="#00CC00" />
+        <asp:Button runat="server" Text="CLEAR" OnClick="Unnamed1_Click" BorderStyle="Groove" BorderColor="#3366CC" />
+        <asp:Button runat="server" Text="SEARCH" OnClick="Unnamed3_Click" BorderStyle="Groove" BorderColor="#FF3300" />
+        <br />
+      <asp:Label ID="Label1" runat="server" Text="Label" Visible="False"></asp:Label>
+    </asp:Panel>
+
+    <asp:Panel ID="Panel2" runat="server" style="float: right; width: 50%; display: inline;">
+        <br />  
+        <h3 id="heading" runat="server"></h3>
+        <p id="paragraph" runat="server"></p>
+        <br />
+    </asp:Panel>
+
+</asp:Content>
---
+++ ToDoList/Index.aspx.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Web.UI;
+
+
+namespace ToDoList
+{
+    public partial class Index : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack) 
+            {
+                Calendar1.Visible = false;
+                Display();
+            }
+        }
+        protected void ImageButton1_Click(object sender, ImageClickEventArgs e)
+        {
+            if (Calendar1.Visible == true)
+            { Calendar1.Visible = false; }
+            else { Calendar1.Visible = true; }
+        }
+
+        protected void SelectedEvent(object sender, EventArgs e)
+        {
+            TextBox1.Text = Calendar1.SelectedDate.ToString("d");
+            Calendar1.Visible = false;
+        }
+        protected void Display()
+        {
+            string CString = ConfigurationManager.ConnectionStrings["CS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(CString))
+            {
+                SqlCommand cmd = new SqlCommand("select date = (Convert(varchar(101),date,106)), list from list where date= Convert(date,GetDate());", con);
+                con.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                string label = ""; string para= "";
+                while (reader.Read())
+                {
+                   label = reader["date"].ToString();
+                   para += "<br />" + reader["list"].ToString();
+
+                }
+                heading.InnerText = label;
+                paragraph.InnerHtml = para;
+                    /**
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                da.Fill(ds, "list");
+                this.paragraph.InnerText = ds.Tables["list"].Rows[0]["date"].ToString();
+                this.paragraph.InnerText = ds.Tables["list"].Rows[0]["list"].ToString();
+                //  GridView1.DataSource = cmd.ExecuteReader();
+                // GridView1.DataBind();
+                **/
+            }
+
+          }
+
+        protected void Unnamed2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string NewLineStr = TextBox2.Text.Replace("\r\n", "<br />");
+                string CString = ConfigurationManager.ConnectionStrings["CS"].ConnectionString;
+                using (SqlConnection con = new SqlConnection(CString))
+                {
+                    SqlCommand cmd = new SqlCommand("Insert into list (date, list) values ('"+TextBox1.Text+"','"+NewLineStr+"');",con);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                }
+                Label1.Visible = true;
+                Label1.Text = "Successfully Inserted!";
+                Display();
+            }
+            catch (SqlException ex)
+            {
+
+                Label1.Text = ex.ToString();            
+            }
+
+        }
+
+        protected void Unnamed1_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text = "";
+            TextBox2.Text = "";
+            Label1.Text = "";
+        }
+
+        protected void Unnamed3_Click(object sender, EventArgs e)
+        {
+            heading.InnerHtml = "";
+            paragraph.InnerHtml = "";
+            string CString = ConfigurationManager.ConnectionStrings["CS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(CString))
+            {
+                SqlCommand cmd = new SqlCommand("select date = (Convert(varchar(101),date,106)), list from list where date ='" + TextBox1.Text+"';", con);
+                con.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    heading.InnerHtml = reader["date"].ToString();
+                    paragraph.InnerHtml += "<br />" + reader["list"].ToString();                    
+                }
+                // GridView1.DataSource = cmd.ExecuteReader();
+                //GridView1.DataBind();
+           }
+        }
+    }
+}
